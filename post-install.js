2455b07234e2c32784a4b76b347ebf34c605dd36
ZacharyShortSecurrency <zachary.short@securrency.com>
2023-08-16 15:11:33 -0500

    Lock versions of dependencies

--- post-install.js
+++ post-install.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 const nativeStrategyPath = 'node_modules/@truffle/compile-solidity/dist/compilerSupplier/loadingStrategies/Native.js';
 
-const maxBufferNative = '1024 * 1024 * 10';
+const maxBuffer = '1024 * 1024 * 50';
 const maxBufferRequired = '1024 * 1024 * 1024';
 
 const nativeStrategy = fs.readFileSync(nativeStrategyPath, 'utf8');
-const modifiedNativeStrategy = nativeStrategy.replace(maxBufferNative, maxBufferRequired);
+const modifiedNativeStrategy = nativeStrategy.replace(maxBuffer, maxBufferRequired);
 
 fs.writeFileSync(nativeStrategyPath, modifiedNativeStrategy);
