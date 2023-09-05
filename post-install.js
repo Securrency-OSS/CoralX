24eb1c25dbde1f6e5ed495ea614f5fc822e1ed4b
VitaliiBabchenkoSecurrency <vitalii.babchenko@securrency.com>
2023-09-05 16:25:10 +0300

    Fix postinstall script

--- post-install.js
+++ post-install.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 
-const nativeStrategyPath = `${__dirname}/node_modules/@truffle/compile-solidity/dist/compilerSupplier/loadingStrategies/Native.js`;
+try {
+  const nativeStrategyPath = require.resolve("@truffle/compile-solidity/dist/compilerSupplier/loadingStrategies/Native.js");
 
-const maxBuffer = '1024 * 1024 * 50';
-const maxBufferRequired = '1024 * 1024 * 1024';
+  const maxBuffer = '1024 * 1024 * 50';
+  const maxBufferRequired = '1024 * 1024 * 1024';
 
-const nativeStrategy = fs.readFileSync(nativeStrategyPath, 'utf8');
-const modifiedNativeStrategy = nativeStrategy.replace(maxBuffer, maxBufferRequired);
+  const nativeStrategy = fs.readFileSync(nativeStrategyPath, 'utf8');
+  const modifiedNativeStrategy = nativeStrategy.replace(maxBuffer, maxBufferRequired);
 
-fs.writeFileSync(nativeStrategyPath, modifiedNativeStrategy);
+  fs.writeFileSync(nativeStrategyPath, modifiedNativeStrategy);
+} catch (e) {
+  console.info(e);
+}
